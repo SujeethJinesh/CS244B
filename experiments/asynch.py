130282d71c0e76509014b4faa1c5076c2e1d7060
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2024-04-21 19:02:36 -0700

    Refactor experiments param server (#3)
    * Refactor Parameter Server
    
    * Refactor
    
    * Fix param servers

--- experiments/asynch.py
+++ experiments/asynch.py
@@ -1,33 +1,32 @@
 import ray
-from parameter_servers.synchronous import ParameterServer
-from workers.traditional import DataWorker
+from parameter_servers.server_actor import ParameterServer
+from workers.worker_task import compute_gradients
 from models.test_model import ConvNet, get_data_loader, evaluate
 
-iterations = 20_000
+iterations = 200
 num_workers = 2
 
 def run_asynch_experiment():
   ray.init(ignore_reinit_error=True)
   ps = ParameterServer.remote(1e-2)
-  workers = [DataWorker.remote() for i in range(num_workers)]
 
   model = ConvNet()
   test_loader = get_data_loader()[1]
 
   print("Running Asynchronous Parameter Server Training.")
   current_weights = ps.get_weights.remote()
-  gradients = {}
-  for worker in workers:
-      gradients[worker.compute_gradients.remote(current_weights)] = worker
+  gradients = []
+  for _ in range(num_workers):
+      gradients.append(compute_gradients.remote(current_weights))
 
   for i in range(iterations * num_workers):
-      ready_gradient_list, _ = ray.wait(list(gradients))
+      ready_gradient_list, _ = ray.wait(gradients)
       ready_gradient_id = ready_gradient_list[0]
-      worker = gradients.pop(ready_gradient_id)
+      gradients.remove(ready_gradient_id)
 
       # Compute and apply gradients.
       current_weights = ps.apply_gradients.remote(*[ready_gradient_id])
-      gradients[worker.compute_gradients.remote(current_weights)] = worker
+      gradients.append(compute_gradients.remote(current_weights))
 
       if i % 10 == 0:
           # Evaluate the current model after every 10 updates.
---
+++ parameter_servers/server_actor.py
@@ -0,0 +1,72 @@
+import torch
+import numpy as np
+from models.test_model import ConvNet
+import ray
+from workers.worker_task import compute_gradients
+from models.test_model import ConvNet, get_data_loader, evaluate
+
+iterations = 200
+num_workers = 2
+
+@ray.remote
+class ParameterServer(object):
+    def __init__(self, lr):
+        self.model = ConvNet()
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
+
+    def apply_gradients(self, gradients):
+        grad = ray.get(gradients)
+        summed_gradients = [
+            np.stack(gradient_zip).sum(axis=0) for gradient_zip in zip(*grad)
+        ]
+        self.optimizer.zero_grad()
+        self.model.set_gradients(summed_gradients)
+        self.optimizer.step()
+        return self.model.get_weights()
+
+    def get_weights(self):
+        return self.model.get_weights()
+
+    def run_synch_experiment(self):
+      test_loader = get_data_loader()[1]
+
+      print("Running synchronous parameter server training.")
+      current_weights = self.get_weights()
+      for i in range(iterations):
+          gradients = [compute_gradients.remote(current_weights) for _ in range(num_workers)]
+          # Calculate update after all gradients are available.
+          current_weights = self.apply_gradients(gradients)
+
+          if i % 10 == 0:
+              # Evaluate the current model.
+              self.model.set_weights(current_weights)
+              accuracy = evaluate(self.model, test_loader)
+              print("Iter {}: \taccuracy is {:.1f}".format(i, accuracy))
+
+      print("Final accuracy is {:.1f}.".format(accuracy))
+
+    def run_asynch_experiment(self):
+      test_loader = get_data_loader()[1]
+
+      print("Running Asynchronous Parameter Server Training.")
+      current_weights = self.get_weights()
+      gradients = []
+      for _ in range(num_workers):
+          gradients.append(compute_gradients.remote(current_weights))
+
+      for i in range(iterations * num_workers):
+          ready_gradient_list, _ = ray.wait(gradients)
+          ready_gradient_id = ready_gradient_list[0]
+          gradients.remove(ready_gradient_id)
+
+          # Compute and apply gradients.
+          current_weights = self.apply_gradients([ready_gradient_id])
+          gradients.append(compute_gradients.remote(current_weights))
+
+          if i % 10 == 0:
+              # Evaluate the current model after every 10 updates.
+              self.model.set_weights(current_weights)
+              accuracy = evaluate(self.model, test_loader)
+              print("Iter {}: \taccuracy is {:.1f}".format(i, accuracy))
+
+      print("Final accuracy is {:.1f}.".format(accuracy))

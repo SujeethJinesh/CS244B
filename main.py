3887cfca3f470b65cd7175a14ce03dc5e12689ed
zgan1 <74687251+zgan1@users.noreply.github.com>
2024-04-23 23:07:25 -0700

    try driver disrupting training with threads

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ray
 import time
+import threading
 
 from experiments.synch import run_synch_experiment
 from experiments.asynch import run_asynch_experiment
@@ -14,14 +15,25 @@ def main():
 
   # Run asynchronous param server experiment
   ray.init(ignore_reinit_error=True)
-  ps = ParameterServer.options(max_concurrency=2).remote(1e-2)
+  ps = ParameterServer.remote(1e-2)
   # ray.get([ps.run_asynch_experiment.remote()])
+  ray.get([ps.run_synch_experiment.remote()])
 
-  try: 
-    ray.get([ps.run_synch_experiment.remote(), ps.exit.remote(10)])
-  except:
-    print("An exception occured")
-    
+  # ps = ParameterServer.options(max_concurrency=2).remote(1e-2)
+  # try: 
+  #   ray.get([ps.run_synch_experiment.remote(), ps.exit.remote(10)])
+  # except:
+  #   print("An exception occured")
+
+  # def disrupt_training(actor_handle):
+  #   time.sleep(10)
+  #   print("disrupt thread wakes up")
+  #   ray.kill(actor_handle)
+
+  # thread = threading.Thread(target=disrupt_training, args=(ps,))
+  # thread.start()
+  # thread.join()
+
   print("Driver exits")
 
 if __name__ == "__main__":

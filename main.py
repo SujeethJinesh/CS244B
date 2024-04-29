4198b54e5c2f20e9a71e0e1bcbb4891ecc6d2f4b
Sherry Luo <sherry.z.luo123@berkeley.edu>
2024-04-28 19:49:45 -0700

    working recovery but needs global actor

--- main.py
+++ main.py
@@ -1,44 +1,37 @@
 import ray
 import time
 import threading
+import os
 
 from experiments.synch import run_synch_experiment
 from experiments.asynch import run_asynch_experiment
 from parameter_servers.server_actor import ParameterServer
 from parameter_servers.server_killer import kill_server
+from parameter_servers.model_saver import ModelSaver
 
 LEARNING_RATE = 1e-2
 SYNCHRONOUS = True
 
-def run_experiment_with_no_ckpointing(ps: ParameterServer):
+def run_experiment_with_no_ckpointing():
+  ps = ParameterServer.remote(LEARNING_RATE)
   ray.get([ps.run_training.remote(SYNCHRONOUS)])
 
-def run_experiment_with_object_store_ckpointing(ps: ParameterServer, ckpoint_period_sec: float = 10):
-  try:
-    done_id, result_ids = [], [ps.run_training.remote(SYNCHRONOUS)]
-    current_weights = ray.get([ps.get_weights.remote()])
-    current_weights_ref = ray.put(*current_weights)
-    print(current_weights)
-    print(done_id)
-    while len(done_id) < 1:
-      print('saving weights')
-      done_id, result_ids = ray.wait(result_ids, timeout=ckpoint_period_sec)
-      print(done_id, result_ids)
-      current_weights = ray.get([ps.get_weights.remote()])
-      current_weights_ref = ray.put(*current_weights)
-      print('saving weights')
-      print(current_weights_ref)
-  except:
-    print('catching exception')
-    # Initialize new ParameterServer.
-    ps = ParameterServer.remote(LEARNING_RATE)
-    print('making new parameter server')
-    if current_weights_ref:
-      # Recover from checkpoint.
-      print('Recovering')
-      ray.get([ps.set_weights.remote(current_weights_ref)])
-      print('running again')
-      run_experiment_with_object_store_ckpointing(ps)
+
+def run_experiment_with_object_store_ckpointing(ckpoint_period_sec: float = 10):
+  ms = ModelSaver.remote()
+  def _run_experiment(first_run=True):
+    try:
+      ps = ParameterServer.remote(LEARNING_RATE, ms)
+      if not first_run:
+        ray.get(ps.set_weights.remote(ms.get_weights.remote(), ms.get_iteration_count.remote()))
+      else:
+        server_killer_ref = kill_server.remote([ps], 30)
+      ray.get(ps.run_training.remote(SYNCHRONOUS))
+    except Exception as e:
+      print('catching exception', e)
+      ps = None
+      _run_experiment(first_run=False)
+  _run_experiment()
     
 
 def run_experiment_with_disk_ckpointing(ps: ParameterServer):
@@ -54,13 +47,11 @@ def main():
   # run_asynch_experiment()
 
   # Run asynchronous param server experiment
-  ray.init(ignore_reinit_error=True)
-  ps = ParameterServer.remote(LEARNING_RATE)
+  ray.init()
+  
   # ray.get([ps.run_asynch_experiment.remote()])
 
-  # server_killer_ref = kill_server.remote([ps], 30)
-
-  run_experiment_with_object_store_ckpointing(ps)
+  run_experiment_with_object_store_ckpointing()
   # ray.get(server_killer_ref)
 
   # ps = ParameterServer.options(max_concurrency=2).remote(1e-2)
@@ -70,6 +61,7 @@ def main():
   #   print("An exception occured")
 
   print("Driver exits")
+
 
 if __name__ == "__main__":
   main()
--- parameter_servers/server_actor.py
+++ parameter_servers/server_actor.py
@@ -12,9 +12,11 @@ num_workers = 2
 
 @ray.remote(max_restarts=0)
 class ParameterServer(object):
-    def __init__(self, lr):
+    def __init__(self, lr, model_saver):
         self.model = ConvNet()
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
+        self.model_saver = model_saver
+        self.start_iteration = 0
 
     def apply_gradients(self, gradients):
         grad = ray.get(gradients)
@@ -26,8 +28,11 @@ class ParameterServer(object):
         self.optimizer.step()
         return self.model.get_weights()
 
-    def set_weights(self, weights):
-        self.model.set_weights(weights)
+    def set_weights(self, weights, iteration_count):
+        if weights:
+          self.model.set_weights(weights)
+          self.model_saver.set_weights_iteration_count.remote(weights, iteration_count)
+          self.start_iteration = iteration_count
 
     def get_weights(self):
         return self.model.get_weights()
@@ -57,19 +62,16 @@ class ParameterServer(object):
 
       print("Running synchronous parameter server training.")
       current_weights = self.get_weights()
-      for i in range(iterations):
+      for i in range(self.start_iteration, iterations):
           gradients = [compute_gradients.remote(current_weights) for _ in range(num_workers)]
           # Calculate update after all gradients are available.
           current_weights = self.apply_gradients(gradients)
 
           if i % 10 == 0:
               # Evaluate the current model.
-              self.model.set_weights(current_weights)
+              self.set_weights(current_weights, i)
               accuracy = evaluate(self.model, test_loader)
               print("Iter {}: \taccuracy is {:.1f}".format(i, accuracy))
-
-          if i == 50:
-            os._exit(0)
 
       print("Final accuracy is {:.1f}.".format(accuracy))
 
@@ -82,7 +84,7 @@ class ParameterServer(object):
       for _ in range(num_workers):
           gradients.append(compute_gradients.remote(current_weights))
 
-      for i in range(iterations * num_workers):
+      for i in range(self.start_iteration, iterations * num_workers):
           ready_gradient_list, _ = ray.wait(gradients)
           ready_gradient_id = ready_gradient_list[0]
           gradients.remove(ready_gradient_id)
@@ -93,7 +95,7 @@ class ParameterServer(object):
 
           if i % 10 == 0:
               # Evaluate the current model after every 10 updates.
-              self.model.set_weights(current_weights)
+              self.set_weights(current_weights, i)
               accuracy = evaluate(self.model, test_loader)
               print("Iter {}: \taccuracy is {:.1f}".format(i, accuracy))
 

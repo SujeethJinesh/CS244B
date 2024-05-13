4e5ae3c2dd1af0c7538d2a0edfd6b5d72ebb90b8
Sherry Luo <sherry.z.luo123@berkeley.edu>
2024-05-12 20:05:59 -0700

    running on cloud see README

--- README.md
+++ README.md
@@ -7,3 +7,121 @@ To activate the virtual environment, use the following command
 ```
 source env/bin/activate
 ```
+
+## Zookeeper
+
+A zookeeper installation script is provided at `zookeeper/zookeeper_setup.sh`. After running this script, `apache-zookeeper-3.8.4-bin` should appear in the top-level of this project.
+
+A running zookeeper server is needed before start the chain node experiments.
+
+A local zookeeper server can be started by running:
+   ```
+   ./apache-zookeeper-3.8.4-bin/bin/zkServer.sh start
+   ```
+
+To stop the zookeeper server, run
+   ```
+   ./apache-zookeeper-3.8.4-bin/bin/zkServer.sh stop
+   ```
+
+To get debug information from the zookeeper server, you can also run the server
+in the foreground.
+   ```
+   ./apache-zookeeper-3.8.4-bin/bin/zkServer.sh start-foreground
+   ```
+
+By default, the zookeeper clients used in the chain node experiments listen to
+localhost at `127.0.0.1:2181`.
+
+## Metrics
+
+Steps for setting up metrics monitoring on a single-node local cluster:
+
+1. Download [Prometheus](https://prometheus.io/download/). Unzip the 
+   archive into a local directory.
+
+2. Download [Grafana](https://grafana.com/grafana/download). Unzip the 
+   archive into a local directory.
+
+3. Start the ray cluster experiment, e.g. `python main.py`.
+
+4. Open the ray dashboard in the browser. The default address is 
+   `localhost:8265`.
+
+5. In a new terminal, navigate to the Prometheus directory (e.g. 
+   `prometheus-2.52.0-rc.1.darwin-amd64`), run:
+   ```
+   ./prometheus --config.file=/tmp/ray/session_latest/metrics/prometheus/prometheus.yml
+   ```
+   This starts a service that records metrics from ray.
+
+6. In another new terminal, navigate to the Grafana directory (e.g. 
+   `/grafana-v10.4.2/`), run:
+   ```
+   ./bin/grafana server --config /tmp/ray/session_latest/metrics/grafana/grafana.ini web`
+   ```
+   This starts the Grafana server to visualize the metrics with 
+   presets.
+
+7. Open `localhost:3000` in the browser to access Grafana. The 
+   "Cluster Utilization" chart in the Serve Dashboard is often the 
+   most relevant.
+
+8. Open `localhost:9090` in the browser to access Prometheus. It 
+   provides more granular dashboarding/metric monitoring than Grafana. 
+   Some useful queries include `ray_node_cpu_utilization`, 
+   `ray_node_mem_used`, `ray_component_uss_mb`. The full list of 
+   metrics is defined in [the official ray doc](https://docs.ray.io/en/latest/ray-observability/reference/system-metrics.html).
+
+9.  Check the node details in the Ray dashboard to see resource usage
+    for individual workers.
+
+10. (Optional) By default, metrics are collected every 10 seconds. To 
+    change this, edit `/tmp/ray/session_latest/metrics/prometheus/prometheus.yml`. 
+    Setting a long interval might make it harder to quickly notice
+    issues like dead worker nodes.
+
+## GCP
+
+You must first authenticate by installing `gcloud` using https://cloud.google.com/sdk/docs/install and running:
+
+```
+gcloud auth application-default login
+```
+
+Next, run the following:
+
+```
+cd /path/to/CS244B
+
+gcloud config set project ft-distributed-ml-training
+
+ray up gcp.yaml
+
+# View the dashboard at http://127.0.0.1:8265/ after running:
+ray dashboard /path/to/CS244B/gcp.yaml
+
+# In a separate terminal, run:
+ray job submit --runtime-env-json='{"excludes": ["/.git/"]}' --working-dir . -- python main.py
+```
+
+Useful commands:
+```
+  # To terminate the cluster:
+    ray down gcp.yaml
+  
+  # To retrieve the IP address of the cluster head:
+    ray get-head-ip gcp.yaml
+  
+  # To port-forward the cluster's Ray Dashboard to the local machine:
+    ray dashboard gcp.yaml
+  
+  # To submit a job to the cluster, port-forward the Ray Dashboard in another terminal and run:
+    ray job submit --address http://localhost:<dashboard-port> --working-dir . -- python my_script.py
+  
+  # To connect to a terminal on the cluster head for debugging:
+    ray attach gcp.yaml
+  
+  # To monitor autoscaling:
+    ray exec gcp.yaml 'tail -n 100 -f /tmp/ray/session_latest/logs/monitor*'
+```
--- zookeeper/zoo.py
+++ zookeeper/zoo.py
@@ -14,7 +14,7 @@ from kazoo.client import KazooClient
 # TODO: What to do for Training Worker clients?
 
 class KazooChainNode(object):
-    def __init__(self, node_id, init_role, hosts='127.0.0.1:2181'):
+    def __init__(self, node_id, init_role, prev_node_change_callback, hosts='127.0.0.1:2181'):
         self.zk = KazooClient(hosts=hosts)
         self.zk.start()
         self.head = False
@@ -23,35 +23,49 @@ class KazooChainNode(object):
         self.node_id = -1
         self.next_id = -1
         self.setup_node(node_id, init_role)
+        self.prev_node_change_callback = prev_node_change_callback
 
-    def handle_delete_event(self, event):
-        print("event is", event)
-        if event.type=='DELETED':
+    def handle_delete_or_change_event(self, event):
+        print("Node ",  self.node_id, " handles event. Event is", event)
+        node_id = int(event.path[6])
+        if event.type=='CHANGED':
+            print("Node", str(self.node_id), "handle changed event")
+            # print(self.zk.get("/base/" + str(node_id)))
+            
+        elif event.type=='DELETED':
+            print("In handle DELTED")
             # get node id
             if int(event.path[6]) < self.node_id:
                 # check if self is the new head
-                self._get_largest_smaller
+                self._get_largest_smaller()
             else:
                 # check if self is the new tail
-                self._get_smallest_larger
+                self._get_smallest_larger()
+        
+        self.prev_node_change_callback(event)
+        print("after event callback")
 
     def handle_child_event(self, event):
         print("event is", event)
         if event.type == "CHILD":
-            self._get_smallest_larger
+            self._get_neighbors()
 
     def _get_largest_smaller(self):
         largest_smaller = None
         for node in self.zk.get_children("/base"):
+            print("get a list of children")
+            print("child is ", node)
             if int(node) < self.node_id:
                 if largest_smaller is None or int(node) > largest_smaller:
                     largest_smaller = int(node)
         if largest_smaller is None:
+            print("set head")
             self.set_head()
         else:
+            print("not head")
             self.head = False
             self.prev_id = largest_smaller
-            self.zk.exists("/base/"+str(self.prev_id), watch=self.handle_delete_event)
+            self.zk.exists("/base/"+str(self.prev_id), watch=self.handle_delete_or_change_event)
 
     def _get_smallest_larger(self):
         smallest_larger = None
@@ -64,14 +78,9 @@ class KazooChainNode(object):
         else: 
             self.tail = False
             self.next_id = smallest_larger
-            self.zk.exists("/base/"+str(self.next_id), watch=self.handle_delete_event)
+            self.zk.exists("/base/"+str(self.next_id), watch=self.handle_delete_or_change_event)
     
-    def setup_node(self, node_id, init_role):
-        print("NODE ", node_id, " is created")
-        
-        self.node_id = node_id
-        self.zk.create("/base/"+str(self.node_id), b"somevalue", ephemeral=True, makepath=True)
-
+    def _get_neighbors(self):
         largest_smaller = None
         smallest_larger = None
         for node in self.zk.get_children("/base"):
@@ -87,21 +96,58 @@ class KazooChainNode(object):
         else:
             self.head = False
             self.prev_id = largest_smaller
-            self.zk.exists("/base/"+str(self.prev_id), watch=self.handle_delete_event)
+            if not self.zk.exists("/base/"+str(self.prev_id), watch=self.handle_delete_or_change_event):
+                # retry if there is a state change since `get_children`
+                self._get_neighbors()
         
         if smallest_larger is None:
             self.set_tail()
         else:
             self.tail = False
             self.next_id = smallest_larger
-            self.zk.exists("/base/"+str(self.next_id), watch=self.handle_delete_event)
+            if not self.zk.exists("/base/"+str(self.next_id), watch=self.handle_delete_or_change_event):
+                # retry if there is a state change since `get_children`
+                self._get_neighbors()
+
+    def setup_node(self, node_id, init_role):
+        print("NODE ", node_id, " is created")
+        
+        self.node_id = node_id
+        self.zk.create("/base/"+str(self.node_id), b"somevalue", ephemeral=True, makepath=True)
+
+        self._get_neighbors()
+        # largest_smaller = None
+        # smallest_larger = None
+        # for node in self.zk.get_children("/base"):
+        #     if int(node) < self.node_id:
+        #         if largest_smaller is None or int(node) > largest_smaller:
+        #             largest_smaller = int(node)
+        #     elif int(node) > self.node_id:
+        #         if smallest_larger is None or int(node) < smallest_larger:
+        #             smallest_larger = int(node)   
+        
+        # if largest_smaller is None:
+        #     self.set_head()
+        # else:
+        #     self.head = False
+        #     self.prev_id = largest_smaller
+        #     self.zk.exists("/base/"+str(self.prev_id), watch=self.handle_delete_or_change_event):
+        
+        # if smallest_larger is None:
+        #     self.set_tail()
+        # else:
+        #     self.tail = False
+        #     self.next_id = smallest_larger
+        #     self.zk.exists("/base/"+str(self.next_id), watch=self.handle_delete_or_change_event)
 
     def stop(self):
         self.zk.stop()
     
     def set_head(self):
         self.head = True
+        print("New head is node ", self.node_id)
         self.prev_id = -1
+        self.zk.get_children("/base", watch=self.handle_child_event)
 
     def set_tail(self):
         self.tail = True

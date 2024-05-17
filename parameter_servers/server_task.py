6b14f8959fe38e29d77ec0cf071b94f8d9693343
SujeethJinesh <sujeethjinesh@gmail.com>
2024-05-17 10:50:03 -0700

    Drill into experiment 4

--- parameter_servers/server_task.py
+++ parameter_servers/server_task.py
@@ -7,7 +7,7 @@ import os
 import threading
 from models.test_model import ConvNet, get_data_loader, evaluate
 from kazoo.client import KazooClient
-from kazoo.exceptions import NodeExistsError
+from kazoo.exceptions import NodeExistsError, NoNodeError
 
 iterations = 400
 weight_update_frequency = 10
@@ -20,14 +20,18 @@ class ParamServerTaskActor:
 
   def run_parameter_server_task(self, model, num_workers, lr, weight_saver):
     print("Parameter Server is starting")
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-
     then = time.time()
     test_loader = get_data_loader()[1]
 
     zk = KazooClient(hosts='127.0.0.1:2181', timeout=1.0)
     zk.start()
     zk.create("/base/parameter_server", b"", ephemeral=True, makepath=True)
+
+    retrieved_data = zk.get("/base/weights")
+    unpickled_w_string = ray.cloudpickle.loads(retrieved_data[0])
+    stored_weights = ray.get(unpickled_w_string)
+    model.set_weights(stored_weights)
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 
     def maybe_retrieve_gradients_from_zk(event):
       nonlocal zk
@@ -59,11 +63,13 @@ class ParamServerTaskActor:
         except NodeExistsError:
           # We'll get em next time
           print(f"PS Lock on node {worker_index} was locked, we'll get em next time.")
+        except NoNodeError:
+          print(f"No node found, pass")
       return None
 
     def apply_gradients(grad):
       nonlocal model, optimizer
-      # print(f"Applying gradient to weight")
+      # print(f"Applying gradients of length {len(grad)}")
       if grad:
         summed_gradients = [
             np.stack(gradient_zip).sum(axis=0) for gradient_zip in zip(*grad)
@@ -83,23 +89,26 @@ class ParamServerTaskActor:
 
     def evaluate_model():
       nonlocal then, model, test_loader
-      now = time.time()
-      if now - then > 1.0:
-        # Evaluate the current model after every 10 seconds.
-        accuracy = evaluate(model, test_loader)
-        print("accuracy is {:.1f}".format(accuracy))
-        then = now
+      accuracy = evaluate(model, test_loader)
+      print("accuracy is {:.1f}".format(accuracy))
 
     def handle_gradient_update(event):
+      nonlocal then
       weights = None
       gradients = maybe_retrieve_gradients_from_zk(event)
       if gradients:
         weights = apply_gradients(gradients)
       if weights:
         store_weights_in_zookeeper(weights)
-        evaluate_model()
+        now = time.time()
+        if now - then > 1.0:
+          evaluate_model()
+          then = now
 
       zk.exists(event.path, watch=handle_gradient_update)
+
+    print("Running initial evaluation")
+    evaluate_model()
 
     for worker_index in range(num_workers):
       zk.exists(f"/base/gradients/lock/{worker_index}", watch=handle_gradient_update)

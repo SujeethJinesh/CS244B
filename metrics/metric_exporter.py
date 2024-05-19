5c59ef6e377a83934dd630debacd99d4004bdfc4
zgan1 <74687251+zgan1@users.noreply.github.com>
2024-05-19 16:20:41 -0700

    Add metric exporter for custom dashboard metrics
    This dashboard currently has accuracy.

---
+++ metrics/metric_exporter.py
@@ -0,0 +1,25 @@
+import ray
+from ray.util.metrics import Gauge
+
+@ray.remote
+class MetricExporter:
+    def __init__(self, experiment_name):
+        """
+        To instrument custom metrics, add Counter, Gauge, or Histogram field 
+        into this global actor and add a corresponding set method. Then pass it
+        to Ray experiment actors/tasks. Corresponding Prometheus metrics will be
+        collected.
+        
+        Parameters:
+        experiment_name (str): The name of the experiment for tagging the 
+        metrics.
+        """
+        self.accuracy_gauge = Gauge(
+            "training_accuracy",
+            description="Accuracy of the current training run.",
+            tag_keys=("Experiment",)
+        )
+        self.accuracy_gauge.set_default_tags({"Experiment": experiment_name})
+
+    def set_accuracy(self, accuracy):
+        self.accuracy_gauge.set(accuracy)

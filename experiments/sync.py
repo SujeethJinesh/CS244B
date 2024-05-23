3f601d0761ec90b4a79d977a0d15283dc9898c26
SujeethJinesh <sujeethjinesh@gmail.com>
2024-05-22 20:25:44 -0700

    Minor sync exp changes

--- experiments/sync.py
+++ experiments/sync.py
@@ -13,16 +13,16 @@ def run_sync(model, num_workers=1, epochs=5, server_kill_timeout=10, server_reco
 
   print("Running synchronous parameter server training.")
   current_weights = ps.get_weights.remote()
-  for i in range(iterations):
-      gradients = [compute_gradients.remote(current_weights) for _ in range(num_workers)]
-      # Calculate update after all gradients are available.
-      current_weights = ps.apply_gradients.remote(gradients)
+  for i in range(iterations * epochs):
+    gradients = [compute_gradients.remote(current_weights) for _ in range(num_workers)]
+    # Calculate update after all gradients are available.
+    current_weights = ps.apply_gradients.remote(gradients)
 
-      if i % 10 == 0:
-          # Evaluate the current model.
-          model.set_weights(ray.get(current_weights))
-          accuracy = evaluate(model, test_loader)
-          print("Iter {}: \taccuracy is {:.1f}".format(i, accuracy))
+    if i % 10 == 0:
+        # Evaluate the current model.
+        model.set_weights(ray.get(current_weights))
+        accuracy = evaluate(model, test_loader)
+        print("Iter {}: \taccuracy is {:.1f}".format(i, accuracy))
 
   print("Final accuracy is {:.1f}.".format(accuracy))
   # Clean up Ray resources and processes before the next example.

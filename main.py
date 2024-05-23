c6745f5217af247e6409e2747bea43962e61215c
SujeethJinesh <sujeethjinesh@gmail.com>
2024-05-22 21:07:04 -0700

    Get Versioning correct

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def init_random_seeds(seed=0):
 def main():
   # Initialize Ray as this is common between all experiments.
   print("Initializing Ray")
-  ray.init(ignore_reinit_error=True, _metrics_export_port=8081)
+  print(ray.init(ignore_reinit_error=True, _metrics_export_port=8081))
 
   # Ensure consistency across experiments when it comes to randomness
   init_random_seeds()

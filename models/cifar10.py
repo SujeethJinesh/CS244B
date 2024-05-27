b1cbceaa18e81d0da6b368c667ebea4c8cac5109
SujeethJinesh <sujeethjinesh@gmail.com>
2024-05-27 11:28:35 -0700

    Fix CIFAR10 download issue

--- models/cifar10.py
+++ models/cifar10.py
@@ -14,10 +14,10 @@ def get_data_loader():
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])
 
-    train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform)
+    train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform, download=True)
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-    test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform)
+    test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform, download=True)
     test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
     return train_loader, test_loader
 
